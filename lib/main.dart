16be27763b4db1ce809ff7ffbdfe797ac028289f
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-11-16 17:40:34 +0900

    :sparkles: Add connection from login screen to dice screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_example/screens/login_screen.dart';
+import 'package:login_example/screens/s_login.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/screens/s_dice.dart
+++ lib/screens/s_dice.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 
 class DiceScreen extends StatefulWidget {
@@ -11,7 +14,28 @@ class DiceScreen extends StatefulWidget {
   State<DiceScreen> createState() => _DiceScreenState();
 }
 
-class _DiceScreenState extends State<DiceScreen> {
+class _DiceScreenState extends State<DiceScreen> with AfterLayoutMixin<DiceScreen> {
+  @override
+  FutureOr<void> afterFirstLayout(BuildContext context) async {
+    await Future.delayed(Duration(milliseconds: 500), () {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Welcome'),
+              content: Text('Welcome ${widget.id}'),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('OK'))
+              ],
+            );
+          });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
