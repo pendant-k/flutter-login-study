12fe3698fb86a19c86c4c6742833ebabd4942c5f
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-11-16 17:59:36 +0900

    :sparkles: Add dice screen

--- lib/screens/s_dice.dart
+++ lib/screens/s_dice.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,8 @@ class DiceScreen extends StatefulWidget {
 }
 
 class _DiceScreenState extends State<DiceScreen> with AfterLayoutMixin<DiceScreen> {
+  final random = Random();
+  var currentDice = 1;
   @override
   FutureOr<void> afterFirstLayout(BuildContext context) async {
     await Future.delayed(Duration(milliseconds: 500), () {
@@ -23,7 +26,7 @@ class _DiceScreenState extends State<DiceScreen> with AfterLayoutMixin<DiceScree
           builder: (context) {
             return AlertDialog(
               title: Text('Welcome'),
-              content: Text('Welcome ${widget.id}'),
+              content: Text('Hello ${widget.id}'),
               actions: [
                 TextButton(
                     onPressed: () {
@@ -38,6 +41,40 @@ class _DiceScreenState extends State<DiceScreen> with AfterLayoutMixin<DiceScree
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dice by ${widget.id}'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100,
+              ),
+              Image.asset(
+                'assets/images/dice${currentDice}.png',
+                width: 300,
+                height: 300,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    currentDice = random.nextInt(5) + 1;
+                  });
+                },
+                icon: Icon(
+                  Icons.refresh_rounded,
+                  size: 60,
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
